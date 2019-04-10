d233a04bdd10ecc7336de85dff53f11638d3e26f
James Flight <jamesflight2006@gmail.com>
2019-04-10 15:13:17 +0100

    Product split

--- code.js
+++ code.js
@@ -9,12 +9,11 @@ function init(allRateCards) {
       title.innerHTML = el;
       document.getElementById("wrapper").appendChild(title);
       document.getElementById("wrapper").appendChild(node);
-      createDiagram(node.id, getDaigramDataFromRateCards([])); 
+      createDiagram(node.id, getDaigramDataFromRateCards(el, allRateCards)); 
     })
 }
 
 function createDiagram(divId, data) {
-  console.log(divId);
   var $ = go.GraphObject.make;
   var myDiagram =
     $(go.Diagram, divId,
@@ -44,13 +43,27 @@ export function getUniqueKeys(x) {
     return x + 1;
 }
 
-function getDaigramDataFromRateCards(rateCards) {
-  return [
-    { key: "1",              name: "New Build?", color: "red", net: "Net: £40"   },
-    { key: "2", parent: "1", name: "Demeter", color: "green"    },
-    { key: "3", parent: "1", name: "Copricat", color: "red"   },
-    { key: "4", parent: "3", name: "Jellylorum", color: "yellow" },
-    { key: "5", parent: "3", name: "Alonzo", color: "red"     },
-    { key: "6", parent: "2", name: "Munkustrap", color: "red" }
-  ];
+function getDaigramDataFromRateCards(feeType, rateCards) {
+  var rateCardsFilteredByFeeType = R.filter((card) => card[2] === feeType, rateCards);
+  var groups = getProductGroups(rateCardsFilteredByFeeType);
+  return groups.map((group, index) => {
+    return {
+      key: index.toString(),
+      name: group[0][1] + '?',
+      color: 'blue',
+    };
+  }, groups);
+  // return [
+  //   { key: "1",              name: "New Build?", color: "red", net: "Net: £40"   },
+  //   { key: "2", parent: "1", name: "Demeter", color: "green"    },
+  //   { key: "3", parent: "1", name: "Copricat", color: "red"   },
+  //   { key: "4", parent: "3", name: "Jellylorum", color: "yellow" },
+  //   { key: "5", parent: "3", name: "Alonzo", color: "red"     },
+  //   { key: "6", parent: "2", name: "Munkustrap", color: "red" }
+  // ];
+}
+
+function getProductGroups(rateCards) {
+  var productTypes = R.uniq(R.map((el) => el[1], rateCards));
+  return R.map((type) => R.filter((card) => card[1] === type, rateCards), productTypes);
 }
