259c0088fced8c69cc6ec942766b5b22d840e7ea
James Flight <jamesflight2006@gmail.com>
2019-04-11 11:34:11 +0100

    Commit

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+var concatAll = R.reduce(R.concat, []);
+
 function init(allRateCards) {
     var feeTypes = R.uniq(R.map((el) => el[2], allRateCards));
 
@@ -29,8 +31,9 @@ function createDiagram(divId, data) {
     new go.Binding("fill", "color")),
   $(go.Panel, "Table",
     { defaultAlignment: go.Spot.Left, margin: 10 },
-    $(go.TextBlock, { row: 1, column: 1, font: '20pt sans-serif' }, new go.Binding("text", "name")),
-    $(go.TextBlock, { row: 2, column: 1, font: '20pt sans-serif' }, new go.Binding("text", "net")),
+    $(go.TextBlock, { row: 1, column: 1, font: '15pt sans-serif' }, new go.Binding("text", "name")),
+    $(go.TextBlock, { row: 2, column: 1, font: '15pt sans-serif' }, new go.Binding("text", "gross")),
+    $(go.TextBlock, { row: 3, column: 1, font: '15pt sans-serif' }, new go.Binding("text", "per_applicant")),
   )
 );
   
@@ -39,20 +42,23 @@ function createDiagram(divId, data) {
   myDiagram.model = model;
 }
 
-export function getUniqueKeys(x) {
-    return x + 1;
-}
-
 function getDaigramDataFromRateCards(feeType, rateCards) {
   var rateCardsFilteredByFeeType = R.filter((card) => card[2] === feeType, rateCards);
   var groups = getProductGroups(rateCardsFilteredByFeeType);
-  return R.chain(x => x, groups.map((group, index) => {
-    return [{
-      key: index.toString(),
-      name: group[0][1] + '?',
-      color: 'blue',
-    }];
-  }, groups));
+  return concatAll([
+    [{
+      key: 'start',
+      name: 'Start',
+      color: 'yellow'
+    }],
+    getMapForProductGroups(groups),
+    [{
+      parent: 'start',
+      key: 'not_for_product',
+      name: 'Otherwise no fee.',
+      color: 'red'
+    }],
+  ]);
   // return [
   //   { key: "1",              name: "New Build?", color: "red", net: "Net: £40"   },
   //   { key: "2", parent: "1", name: "Demeter", color: "green"    },
@@ -66,4 +72,45 @@ function getDaigramDataFromRateCards(feeType, rateCards) {
 function getProductGroups(rateCards) {
   var productTypes = R.uniq(R.map((el) => el[1], rateCards));
   return R.map((type) => R.filter((card) => card[1] === type, rateCards), productTypes);
+}
+
+function getMapForProductGroups(groups) {
+  return R.chain(x => x, groups.map((group, index) => {
+    return R.concat([{
+      parent: 'start',
+      key: 'product_' + group[0][1].replace(/\s+/g, '-').toLowerCase(),
+      name: 'Quote is for a ' + group[0][1],
+      color: 'blue',
+    }], getMapForExtraConditionsRateCards(getExtraConditionsGroups(group)));
+  }, groups));
+}
+
+function getExtraConditionsString(rateCard) {
+  return R.join('_', [rateCard[9], rateCard[10], rateCard[11], rateCard[12], rateCard[13], rateCard[14], rateCard[15]]);
+}
+
+function getExtraConditionsGroups(rateCards) {
+  var uniqueCombinationsOfConditions = R.uniq(R.map((card) => getExtraConditionsString(card), rateCards));
+  return R.map((condition) => {
+    return R.filter((card) => getExtraConditionsString(card) == condition, rateCards)
+  }, uniqueCombinationsOfConditions);
+}
+
+function getMapForExtraConditionsRateCards(rateCards) {
+  console.log(rateCards);
+  return rateCards.length === 1
+    && rateCards[0].length === 1
+    && getExtraConditionsString(rateCards[0][0]) === '_______'
+      ? [createRateCardMap(rateCards[0][0])]
+      : [];
+}
+
+function createRateCardMap(rateCard) {
+  return {
+    parent: 'product_' + rateCard[1].replace(/\s+/g, '-').toLowerCase(),
+    key: 'extra_conditions_' + getExtraConditionsString(rateCard),
+    gross: 'Gross Fee: £' + rateCard[5],
+    color: 'green',
+    per_applicant: rateCard[18] ? '(Per applicant)' : undefined
+  };
 }
